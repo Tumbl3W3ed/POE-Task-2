405845f1b4670a044a922692a62ea9148cf49c09
Tumbl3W3ed <71217381+Tumbl3W3ed@users.noreply.github.com>
2020-09-14 14:42:02 +0200

    Q.2.2. Done

---
+++ POE/Hero.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POE
+{
+    class Hero : Tile
+    {
+        protected int Hp;
+        int MaxHP;
+        int Damage;
+        Tile[] HeroVision;
+        public Hero(int y, int x) : base(y, x)
+        {
+        }
+
+        public enum Movement
+        {
+            NoMovement, Up, Down, Left, Right
+        }
+    }
+}
