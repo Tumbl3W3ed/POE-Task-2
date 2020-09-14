75864c5e365e7f29515c409b80dcc7d7188a19e7
Tumbl3W3ed <71217381+Tumbl3W3ed@users.noreply.github.com>
2020-09-14 14:34:08 +0200

    Add project files.

---
+++ POE/Obstacle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POE
+{
+    class Obstacle : Tile
+    {
+        public Obstacle(int y, int x) : base(y, x)
+        {
+        }
+    }
+}
---
+++ POE/Tile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POE
+{
+    abstract class Tile
+    {
+        protected Tile(int y, int x)
+        {
+            Y = y;
+            X = x;
+        }
+
+        public int Y { get; set; }
+        public int X { get; set; }
+
+        public enum TileType
+        {
+            Hero, Enemy, Gold, Weapon
+        }
+
+
+    }
+}
